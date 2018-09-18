5065e64884676060da9f8e0fc58335ee6c3fc057
Alison <zhangyinag@126.com>
2018-09-18 13:30:08 +0800

    calendar

--- src/components/calendar/src/Calendar.vue
+++ src/components/calendar/src/Calendar.vue
@@ -1,6 +1,13 @@
 <template>
     <div :class="[b()]">
-        <div :class="[e('header')]"></div>
+        <div :class="[e('header')]">
+            <v-select v-model="year">
+                <v-option v-for="year in yearOpts" :key="year" :label="year">{{year}} 年</v-option>
+            </v-select>
+            <v-select v-model="month" v-if="mode === 'month'">
+                <v-option v-for="month in monthOpts" :key="month" :label="month">{{month + 1}} 月</v-option>
+            </v-select>
+        </div>
         <div :class="[e('body')]">
             <table :class="[e('table')]">
                 <thead v-if="mode === 'month'">
@@ -22,7 +29,6 @@
                                     {{cellText(cell)}}
                                 </div>
                                 <div :class="['cell-content']">
-                                    -
                                 </div>
                             </div>
                         </td>
@@ -34,22 +40,24 @@
 </template>
 
 <script lang="ts">
-import {Component, Emit, Inject, Model, Prop} from 'vue-property-decorator'
+import {Component, Prop} from 'vue-property-decorator'
 import {mixins} from 'vue-class-component'
 import BemMixin from '../../../core/mixins/BemMixin'
 import {Input as VInput} from '../../input'
-import {Popper} from '../../popper/index'
-import {getDaysOfMonth, getRecentDayOfWeek, range} from '../../../utils'
+import {Select as VSelect, Option as VOption} from '../../select'
+import {getRecentDayOfWeek, range} from '../../../utils'
 
 @Component({
-  components: {VInput, Popper},
+  components: {VInput, VSelect, VOption},
   })
 export default class Calendar extends mixins(BemMixin) {
   @Prop({type: Boolean, default: true}) fullscreen!: boolean
 
   @Prop({type: String, default: 'month'}) mode!: 'month'| 'year'
 
-  @Prop(Date) value!: Date
+  year: number = new Date().getFullYear()
+
+  month: number = new Date().getMonth()
 
   get rows (): Date[][] {
     let ret: Date[][] = []
@@ -66,16 +74,25 @@ export default class Calendar extends mixins(BemMixin) {
   }
 
   get date (): Date {
-    return this.value || new Date()
+    return new Date(this.year, this.month)
   }
 
-  get months (): number[] {
+  get months (): Date[] {
     return range(0, 11).map(v => new Date(this.date.getFullYear(), v))
   }
 
-  get days (): number[] {
+  get days (): Date[] {
     let start = getRecentDayOfWeek(new Date(this.date.getFullYear(), this.date.getMonth(), 1), 0)
     return range(0, 41).map(v => new Date(start.getFullYear(), start.getMonth(), start.getDate() + v))
+  }
+
+  get yearOpts () {
+    let current = new Date().getFullYear()
+    return range(current - 10, current + 10)
+  }
+
+  get monthOpts () {
+    return range(0, 11)
   }
 
   cellText (date: Date): string {
